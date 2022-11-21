0843074272ce407494fc63518101653d488a5e3b
wermut_pastis <55725391+wermutpastistb@users.noreply.github.com>
2022-11-22 00:29:25 +0600

    Add wrappers to App.tsx, call setBackgroundColor only on android

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { Platform, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 import { AppNavigator } from '@/navigation';
 import { colors } from '@/constants/colors';
 
 const App = () => {
   StatusBar.setBarStyle('dark-content');
-  StatusBar.setBackgroundColor(colors.white);
+
+  if (Platform.OS === 'android') StatusBar.setBackgroundColor(colors.white);
 
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <BottomSheetModalProvider>
+          <AppNavigator />
+        </BottomSheetModalProvider>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
