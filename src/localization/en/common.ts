ce422dad60daf91045527ae93838768a47cfaf11
elemanjan <mr.eleman@gmail.com>
2022-12-17 14:26:37 +0600

    add localization

---
+++ src/localization/en/common.ts
@@ -0,0 +1,11 @@
+export default {
+  username: 'Username',
+  password: 'Password',
+  email: 'Email',
+  phone: 'Phone',
+  login: 'Login',
+  welcome: 'Welcome',
+  change_language: 'Change Language',
+  forgotPassword: 'Forgot your password?',
+  loginWGoogleFB: 'Login with Google or Facebook',
+}
---
+++ src/localization/ru/common.ts
@@ -0,0 +1,10 @@
+export default {
+  username: 'Имя пользователя',
+  password: 'Пароль',
+  email: 'Почта',
+  phone: 'Телефон',
+  login: 'Логин',
+  welcome: 'Добро пожаловать',
+  change_language: 'Сменить язык',
+  forgotPassword: 'Forgot your password',
+}
